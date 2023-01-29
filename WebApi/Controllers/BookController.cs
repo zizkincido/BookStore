e0482ce32e980ed2603d814c637df18213d64062
zizkincido <49366820+zizkincido@users.noreply.github.com>
2023-01-29 23:50:36 +0300

    Added HttpPut

--- WebApi/Controllers/BookController.cs
+++ WebApi/Controllers/BookController.cs
@@ -1,8 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 namespace WebApi.AddControllers
-
 {
     [ApiController]
     [Route("[controller]s")]
@@ -58,14 +58,31 @@ namespace WebApi.AddControllers
         [HttpPost]
         public IActionResult AddBook([FromBody] Book newBook)
         {
-            var book = BookList.SingleOrDefault(x=>x.Title == newBook.Title);
-            if(book is not null)
+            var book = BookList.SingleOrDefault(x => x.Title == newBook.Title);
+            if (book == null)
+            {
                 return BadRequest();
+            }
             BookList.Add(newBook);
             return Ok();
         }
 
+        [HttpPut("{id}")]
+        public IActionResult UpdateBook(int id,[FromBody] Book updatedBook)
+        {
+            var book = BookList.SingleOrDefault(x=> x.Id == id);
+            if(book == null)
+            {
+                return BadRequest();
+            }
+            book.GenreId = updatedBook.GenreId != default ? updatedBook.GenreId : book.GenreId;
+            book.PageCount = updatedBook.PageCount != default ? updatedBook.PageCount : book.PageCount;
+            book.PublishDate = updatedBook.PublishDate != default ? updatedBook.PublishDate : book.PublishDate;
+            book.Title = updatedBook.Title != default ? updatedBook.Title : book.Title;
+
+            return Ok();
+
+        }
     }
 }
-
     
